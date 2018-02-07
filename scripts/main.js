e0225158cda0ad8b95c4b1b7c28aed67b5874b18
Майоров Антон Валерьевич <vbncmx@mail.ru>
2018-02-08 02:07:55 +0300

    added player and started forming video data object

--- scripts/main.js
+++ scripts/main.js
@@ -16,21 +16,58 @@ var options = {
         "popper": "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min",
         "bootstrap": "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min",
         "bootstrap-tagsinput": "bootstrap-tagsinput.min",
-        "typeahead": "typeahead.bundle"
+        "typeahead": "typeahead.bundle",
+        "youtube": "https://www.youtube.com/iframe_api?noext"
     }
 };
 
 requirejs.config(options);
 
-var fragmentRowTemplate = '<div class=card><div class=card-header id=heading{n} role=tab><h5 class=mb-0><a aria-controls=collapse{n} aria-expanded=true class=fragment-collapse-btn data-parent=#accordion data-toggle=collapse href=#collapse{n}></a><div class="form-group start-end-control-panel"><input class="form-control start-input"type=time value=00:00> <input class="form-control end-input"type=time value=01:00> <button class="btn fa fa-play fragment-play"type=button></button> <span class=fragment-title></span> <button class="btn fa fa-trash-o fragment-delete"type=button></button></div></h5></div><div class="collapse show"id=collapse{n} role=tabpanel aria-labelledby=heading{n}><div class=card-block><div class=form-group><input class="form-control fragment-description" placeholder="Опишите вопрос фрагмента"> <input class="form-control fragment-tags" placeholder="Введите ключевые слова"></div></div></div></div>';
+var fragmentRowTemplate = '<div class=card><div class=card-header id=heading{n} role=tab><h5 class=mb-0><a aria-controls=collapse{n} aria-expanded=true class=fragment-collapse-btn data-parent=#accordion data-toggle=collapse href=#collapse{n}></a><div class="form-group start-end-control-panel"><input step="1" class="form-control start-input"type=time value=00:00> <input step="1" class="form-control end-input"type=time value=01:00> <button class="btn fa fa-play fragment-play"type=button></button> <span class=fragment-title></span> <button class="btn fa fa-trash-o fragment-delete"type=button></button></div></h5></div><div class="collapse show"id=collapse{n} role=tabpanel aria-labelledby=heading{n}><div class=card-block><div class=form-group><input class="form-control fragment-description" placeholder="Опишите вопрос фрагмента"> <input class="form-control fragment-tags" placeholder="Введите ключевые слова"></div></div></div></div>';
 function getFragmentHtml(fragmentId) {
     return fragmentRowTemplate.replace(/{n}/g, fragmentId.toString());
+}
+
+function getFragment(card) {
+    var fragment = {
+        start: card.find(".start-input").val(),
+        end: card.find(".end-input").val(),
+        description: card.find(".fragment-description").val(),
+        tags: card.find(".fragment-tags").val()
+    };
+    return fragment;
+}
+
+function getData(){
+    fragments = [];
+    $(".card").each(function (index) {
+        var card = $(this);
+        var fragment = getFragment(card);
+        fragments.push(fragment);
+    });
+
+    return {
+        fragments: fragments
+    }
+}
+
+function toHhmmss(seconds){
+    var date = new Date(null);
+    date.setSeconds(seconds);
+    var result = date.toISOString().substr(11, 8);
+    return result;
+}
+
+function toSeconds(hhmmss){
+    var a = hhmmss.split(':');    
+    var seconds = (+a[0]) * 60 * 60 + (+a[1]) * 60 + (+a[2]);
+    return seconds;
 }
 
 // http://www.levibotelho.com/development/commit-a-file-with-the-github-api/#5a-the-easy-way
 function saveChanges() {
 
-    var token = "{token}";
+    var token = "22b0810a5802d89e4457b6e928e13e92b2bfa41e";
 
     $.get("https://api.github.com/repos/vbncmx/vbncmx.github.io/git/refs/heads/master", function (data) {
         var headCommitUrl = data.object.url;
@@ -111,14 +148,31 @@ function saveChanges() {
     });
 }
 
+var player;
+window.onYouTubeIframeAPIReady = function(){
+  player = new YT.Player('player', {
+    height: '200',
+    width: '320',
+    videoId: document.getElementById("video-id").value,
+  });
+};
+
 require(["popper"], function (p) {
     window.Popper = p;
     require(["jquery"], function ($) {
-        require(["bootstrap", "bootstrap-tagsinput", "typeahead"], function () {
-
-            saveChanges();
+        require(["bootstrap", "bootstrap-tagsinput", "typeahead"], function () {            
 
             var lastFragmentId = 0;
+            
+            $("#load-yt-video").click(function(){
+                $(".video-id-form").fadeOut(500);
+                $("#fragmenter-panel").fadeIn(500);
+                require(["youtube"]);
+            });
+
+            $("#saveButton").click(function(){
+                saveChanges();
+            });
 
             $("#addFragmentButton").click(function () {
                 $('.collapse').collapse('hide');
@@ -139,8 +193,17 @@ require(["popper"], function (p) {
                     fragmentRow.remove();
                 });
 
+                var startSec = player.getCurrentTime();
+                var endSec = startSec + 60;
+                $(".start-input", fragmentRow).val(toHhmmss(startSec));
+                $(".end-input", fragmentRow).val(toHhmmss(endSec));
+
                 $(".fragment-play", fragmentRow).click(function () {
-                    alert("PLAY");
+                    player.loadVideoById({
+                        'videoId': document.getElementById("video-id").value,
+                        'startSeconds': toSeconds($(".start-input", fragmentRow).val())
+                        // 'endSeconds': currentEnd
+                    });
                 });
 
                 var titleSpan = $(".fragment-title", fragmentRow);
